ddf9e772d0841991ca8f55cf77b6ff0309dd8b5b
Richard Stewing <richard.stewing@udo.edu>
2020-04-18 20:55:04 +0200

    transforming strings

--- src/lib.rs
+++ src/lib.rs
@@ -48,4 +48,63 @@ mod tests {
 
         assert_eq!(deserialized, g);
     }
+
+    #[test]
+    fn date_from_str_s() {
+        let mut s = "12-04-2000:ghg:".to_string();
+        assert_eq!(TagType::date(12,04,2000), TagType::date_from_str(&mut s).unwrap());
+        assert_eq!("ghg:".to_string(), s);
+    }
+
+    #[test]
+    fn date_from_str_f(){
+        let mut s = "f0-04-2000:ghg:".to_string();
+        assert_eq!(None, TagType::date_from_str(&mut s));
+        assert_eq!("f0-04-2000:ghg:".to_string(), s);
+    }
+
+    #[test]
+    fn number_from_str_s(){
+        let mut s = "-2000:ghg:".to_string();
+        assert_eq!(TagType::number(-2000), TagType::number_from_str(&mut s).unwrap());
+        assert_eq!("ghg:".to_string(), s);
+    }
+
+    #[test]
+    fn number_from_str_s2(){
+        let mut s = "2000:ghg:".to_string();
+        assert_eq!(TagType::number(2000), TagType::number_from_str(&mut s).unwrap());
+        assert_eq!("ghg:".to_string(), s);
+    }
+
+    #[test]
+    fn number_from_str_f(){
+        let mut s = "t2000:ghg:".to_string();
+        assert_eq!(None, TagType::number_from_str(&mut s));
+        assert_eq!("t2000:ghg:".to_string(), s);
+    }
+
+    #[test]
+    fn tags_from_str_s(){
+        let mut s = "2000:22-04-2020:ghg:".to_string();
+        let tag = Tag::new().append(TagType::str("ghg".to_string())).append(TagType::date(22,04,2020)).append(TagType::number(2000));
+        assert_eq!(tag, Tag::from_str(&mut s).unwrap());
+        assert_eq!(s, "".to_string());
+    }
+
+    #[test]
+    fn tags_from_str_s2(){
+        let mut s = "2000:22-04-2020:ghg".to_string();
+        let tag = Tag::new().append(TagType::str("ghg".to_string())).append(TagType::date(22,04,2020)).append(TagType::number(2000));
+        assert_eq!(tag, Tag::from_str(&mut s).unwrap());
+        assert_eq!(s, "".to_string());
+    }
+
+    #[test]
+    fn tags_from_str_s3(){
+        let mut s = "2000:22-04-2020:ghg:".to_string();
+        let tag = Tag::new().append(TagType::str("ghg".to_string())).append(TagType::date(22,04,2020)).append(TagType::number(2000));
+        assert_eq!(tag, Tag::from_str(&mut s).unwrap());
+        assert_eq!(s, "".to_string());
+    }
 }
--- src/tags.rs
+++ src/tags.rs
@@ -3,13 +3,13 @@ use super::query::Query;
 
 #[derive(Serialize, Deserialize,Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum TagType {
-    Date(u8,u8,u32),
+    Date(u32,u32,u32),
     Number(i32),
     Str(String),
 }
 
 impl TagType {
-    pub fn date(day : u8, month : u8, year : u32) -> Self {
+    pub fn date(day : u32, month : u32, year : u32) -> Self {
         TagType::Date(day, month, year)
     }
 
@@ -20,6 +20,41 @@ impl TagType {
     pub fn str(s : String) -> Self {
         TagType::Str(s)
     }
+
+    pub fn date_from_str(s : &mut String) -> Option<Self> {
+        match s.clone().chars().collect::<Vec<char>>()[..] {
+            [d1,d2,'-',m1,m2,'-',y1,y2,y3,y4,':',..] => {
+                let d1 = d1.to_digit(10)?;
+                let d2 = d2.to_digit(10)?;
+                let m1 = m1.to_digit(10)?;
+                let m2 = m2.to_digit(10)?;
+                let y1 = y1.to_digit(10)?;
+                let y2 = y2.to_digit(10)?;
+                let y3 = y3.to_digit(10)?;
+                let y4 = y4.to_digit(10)?;
+                let mut s2 = s.split_off(11);
+                std::mem::swap(s, &mut s2);
+                Some(TagType::date(d1*10+d2, m1*10+m2,y1*1000+y2*100+y3*10+y4))
+            },
+            _ => None,
+        }
+    }
+
+    pub fn number_from_str(s : &mut String) -> Option<Self> {
+        match s.find(':') {
+            None => None,
+            Some(n) => {
+                let mut s1 = s.clone();
+                let mut s2 = s1.split_off(n);
+                std::mem::swap(&mut s1, &mut s2);
+                let x : i32 = s2.parse().ok()?;
+                s1.remove(0);
+                std::mem::swap(s, &mut s1);
+                Some(TagType::number(x))
+            }
+        }
+    }
+
 }
 
 #[derive(Serialize, Deserialize,Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -32,6 +67,44 @@ impl Tag {
     pub fn new() -> Self {
         Tag::Nil
     }
+
+    pub fn from_str(s : &mut String) -> Option<Self> {
+        let mut tags : Vec<TagType> = Vec::new();
+        loop {
+            if let Some(t) = TagType::date_from_str(s) {
+                tags.push(t);
+            } else if let Some(t) = TagType::number_from_str(s) {
+                tags.push(t);
+            } else {
+                match s.find(':') {
+                    None => {
+                        tags.push(TagType::Str(s.clone()));
+                        s.clear();
+                        break;
+                    },
+                    Some(n) => {
+                        let mut s1 = s.split_off(n);
+                        tags.push(TagType::Str(s.clone()));
+                        s1.remove(0);
+                        std::mem::swap(s, &mut s1);
+                    },
+                }
+            }
+
+            if s.len() == 0 {
+                break;
+            }
+        }
+
+        let mut t = Tag::new();
+        while let Some(tagt) = tags.pop() {
+            t = t.append(tagt);
+        }
+
+        return Some(t);
+    }
+
+        
     pub fn append(self, t : TagType) -> Self {
         Tag::Cons(t, Box::new(self))
     }
